f24b04f82f6791bb7033fc1ff5877f0b7722faee
mdmmalamin <mdmmalamin@gmail.com>
2024-01-12 00:49:32 +0600

    BB IT House Email Tamplate

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/**/*.html"],
+  theme: {
+    extend: {
+      color: {
+        primary: "#6A3FE2",
+        secondary: "#5A5E63",
+        dark: "#140B2D",
+      },
+    },
+  },
+  plugins: [],
+}
+
